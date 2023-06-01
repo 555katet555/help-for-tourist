6c38e907045accdbc67e70e282528654d6cf1438
555katet555 <zemskova.katena@list.ru>
2023-06-01 10:17:44 +0300

    Интерфейс

--- app/src/main/java/com/example/helper20/MainActivityfinal.java
+++ app/src/main/java/com/example/helper20/MainActivityfinal.java
@@ -3,8 +3,11 @@ package com.example.helper20;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.webkit.WebView;
 import android.widget.Button;
@@ -12,6 +15,13 @@ import android.widget.ListView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.List;
 
 public class MainActivityfinal extends AppCompatActivity {
@@ -20,6 +30,11 @@ public class MainActivityfinal extends AppCompatActivity {
     //TextView textView;
    // WebView webView;
     //String tmp;
+
+    Button btnHit;
+    TextView txtJson;
+    ProgressDialog pd;
+
 
 
 
@@ -32,6 +47,17 @@ public class MainActivityfinal extends AppCompatActivity {
         //webView = findViewById(R.id.webView);
 
        // Intent intent = getIntent();
+
+        btnHit = (Button) findViewById(R.id.btnHit);
+        txtJson = (TextView) findViewById(R.id.tvJsonItem);
+
+        btnHit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new JsonTask().execute("http://92.63.106.150:5000/api/recognize");
+            }
+        });
+
 
 
         Button btn_hm = (Button) findViewById(R.id.btn_hm);
@@ -51,4 +77,73 @@ public class MainActivityfinal extends AppCompatActivity {
         //final ProgressBar progressBar = (ProgressBar) findViewById(R.id.progressBar);
    // }
 
+    private class JsonTask extends AsyncTask<String, String, String> {
+
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+            pd = new ProgressDialog(MainActivityfinal.this);
+            pd.setMessage("Please wait");
+            pd.setCancelable(false);
+            pd.show();
+        }
+
+        protected String doInBackground(String... params) {
+
+
+            HttpURLConnection connection = null;
+            BufferedReader reader = null;
+
+            try {
+                URL url = new URL(params[0]);
+                connection = (HttpURLConnection) url.openConnection();
+                connection.connect();
+
+
+                InputStream stream = connection.getInputStream();
+
+                reader = new BufferedReader(new InputStreamReader(stream));
+
+                StringBuffer buffer = new StringBuffer();
+                String line = "";
+
+                while ((line = reader.readLine()) != null) {
+                    buffer.append(line+"\n");
+                    Log.d("Response: ", "> " + line);   //here u ll get whole response...... :-)
+
+                }
+
+                return buffer.toString();
+
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if (connection != null) {
+                    connection.disconnect();
+                }
+                try {
+                    if (reader != null) {
+                        reader.close();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            if (pd.isShowing()){
+                pd.dismiss();
+            }
+            txtJson.setText(result);
+        }
+    }
 }
+
+
